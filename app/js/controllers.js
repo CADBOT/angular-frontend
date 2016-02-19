3dd50a15ccda5ac031c5fc602d91511cec7c0d3f
CADBOT <charliec364@gmail.com>
2016-02-19 06:33:31 -0800

    add form for new product and form validation/backend

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,18 +1,8 @@
 var storeApp = angular.module('storeApp', []);
 
 storeApp.controller('StoreController', StoreController);
+storeApp.controller('ProductController', ProductController);
 
-/*
- * Usually instead of all of our data being hardcoded into a controller, we 
- * woudl be grabbing it from a backend web server. As angular is a SPA where
- * the templates aren't rendered by the server like in a more traditional app,
- * we must grab this data using an AJAX request (interlude to the whiteboard to
- * draw up the architecture. Show students how to run the server locally or connect
- * to the backup one running on digital ocean
- *
- * Instead of using jquery to do our AJAX request, we will use angulars built-in
- * $http service.
- */
 function StoreController($http) {
   var vm = this;
   vm.store = 'Online Product Store';
@@ -20,22 +10,46 @@ function StoreController($http) {
   
   activate();
 
-  /*
-   * It's considered a good practice to put all controller startup logic in an activate function.
-   * This is useful because it puts all that logic on one place, instead of spreading it about the 
-   * controller at random. It is also nice as it allows you to quickly zerio in on the activation
-   * logic for any controller
-   */
   function activate() {
     $http.get('http://localhost:3000/products').success(function(data) {
       vm.products = data;
     }).catch(function(info) {
-     // Catch enables us to handle errors that may occur, such as the server not being up
-     // (demonstrate how this runs if the server is down
      console.log(info); 
     }).finally(function() {
       // finally will get called after success or catch. Useful for perfoming cleanup operations
       console.log('From finally: I always run');
     })
+  }
+}
+
+function ProductController($http) {
+  var vm = this;
+  vm.name = '';
+  vm.cost = '';
+  vm.errorMessage = '';
+  vm.submit = function() {
+    if (validate()) {
+      console.log('submitted');
+      var data = JSON.stringify({name: vm.name, cost: vm.cost});
+      $http.post('http://localhost:3000/products', data).success(function(data) {
+        console.log(data);
+      });
+    }
+    else {
+      console.log('invalid');
+    }
+  };
+
+  function validate() {
+    if (vm.name == '' || !vm.name) {
+      vm.errorMessage = 'name must be present';
+      return false;
+    }
+    if (vm.cost == '' || !vm.cost) {
+      vm.errorMessage = 'cost must be present';
+      return false;
+    }
+    vm.errorMessage = '';
+    return true;
   }
 }
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var products = [
+    {name: 'iphone 6', cost: 300}, 
+    {name: 'soap', cost: 2},
+    {name: 'juice', cost: 5},
+    {name: 'batteries', cost: 5}
+];
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -9,13 +17,17 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(bodyParser.json());
+
 app.get('/products', function(req, res) {
-  res.json([
-    {name: 'iphone 6', cost: 300}, 
-    {name: 'soap', cost: 2},
-    {name: 'juice', cost: 5},
-    {name: 'batteries', cost: 5}
-  ])
+  res.json(products);
+});
+
+app.post('/products', function(req, res) {
+  console.log(req.body);
+  products.push(req.body);
+  console.log(products);
+  res.json({msg: 'new product received'});
 });
 
 var port = 3000;
