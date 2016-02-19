def0664e11f60b03a547f132143f9329ae55790b
CADBOT <charliec364@gmail.com>
2016-02-19 04:52:44 -0800

    finish lesson 7

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,5 +8,10 @@ storeApp.controller('StoreController', StoreController);
 function StoreController() {
   var vm = this;
   vm.store = 'Online Product Store';
-  vm.products = [{name: 'iphone 6', cost: 300}, {name: 'soap', cost: 2}];
+  vm.products = [
+    {name: 'iphone 6', cost: 300}, 
+    {name: 'soap', cost: 2},
+    {name: 'juice', cost: 5},
+    {name: 'batteries', cost: 5}
+  ];
 }
