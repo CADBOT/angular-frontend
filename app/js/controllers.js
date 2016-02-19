e01318adb97b20b5f2a5bccd339137404cb175fd
CADBOT <charliec364@gmail.com>
2016-02-19 04:24:39 -0800

    finish lesson 6

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,10 @@ var storeApp = angular.module('storeApp', []);
 storeApp.controller('StoreController', StoreController);
 
 /*
- * instead of using $scope we will set the data on the
- * execution context of the contoller instance. It's
- * considered a good practice to alias it to vm for
- * view model (interlude for conceptual discussion about
- * MVC vs MVVM)
+ * Move the data from two objects one one array
  */
 function StoreController() {
   var vm = this;
   vm.store = 'Online Product Store';
-  vm.productOne = {name: 'iphone 6', cost: 300};
-  vm.productTwo = {name: 'soap', cost: 2};
+  vm.products = [{name: 'iphone 6', cost: 300}, {name: 'soap', cost: 2}];
 }
